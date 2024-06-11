2268a3415ca92a0ae5a56ab48faeacd67e235d31
guilhermegouw <guilherme.gouw@gmail.com>
2024-06-11 16:04:43 -0300

    11-06-2024

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ total }}</p>
+    <button @click="calcula('-')">Decrement</button>
+    <button @click="calcula('+')">Increment</button>
+
+    <p>Nome Computado: {{ nomeFormatado }}</p>
+    <label>Input a computar</label>
+    <input v-model="nome" type="text"><br>
+
+    <label>Input a observar</label>
+    <input v-model="busca" type="text">
+    <p v-text="resultado"></p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'lv-contador',
+        data() {
+            return {
+                total: 10,
+                nome: 'jose antonio',
+                resultado: "",
+                busca: "",
+            }
+        },
+        computed: {
+            nomeFormatado: {
+                get: function() {
+                    return this.nome.toUpperCase()
+                },
+                set: function( novoValor ) {
+                    this.nome = novoValor.substring(0, 3)
+                }
+            }
+        },
+        watch: {
+            busca: function() {
+                this.resultado = 'Aguardando o término de digitação...'
+                this.recolheResposta()
+            }
+        },
+        methods: {
+            calcula( sinal ) {
+                this.total = (sinal == '-') ? this.total -1: this.total + 1
+            },
+            recolheResposta() {
+                let valor = this.busca
+                setTimeout(() => {
+                    if (valor == this.busca) 
+                    this.resultado = 'Terminou de digitar...'
+                }, 500)
+            }
+        }
+    }
 </script>
 
 <style>
